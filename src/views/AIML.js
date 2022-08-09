1f567b001cd607340d2aea0a310bfe77a40fc607
Allan-Barnard <94503880+Allan-Barnard@users.noreply.github.com>
2022-08-09 12:46:57 -0500

    Update AIML.js

--- src/views/AIML.js
+++ src/views/AIML.js
@@ -90,7 +90,7 @@ const AIML = () => {
       </Row>
       {/***Sales & Feed***/}
 	  <Row>
-	    <a href="http://18.212.18.67:8080/DataGen/python_datagen/Data_item_output.html">Data: Inbound to AWS</a>
+	    <a href="http://18.212.18.67:8000/cgi-bin/python_datagen/DataGen_1210_Generate_items_initial.py">Data: Inbound to AWS</a>
 	  </Row>
       <Row>
         <Col sm="6" lg="6" xl="7" xxl="8">
