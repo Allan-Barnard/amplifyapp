4db4e8853c634a6d521d8c2a2b800f26e1639aa4
Allan-Barnard <94503880+Allan-Barnard@users.noreply.github.com>
2022-08-26 16:05:20 -0500

    Update, 2022-Aug-26, Allan & Satish
    Walking HTML to REACT to GitHub to AWS to Windows/Python

--- src/App_with_example_HelloWorld.js
+++ src/App_with_example_HelloWorld.js
@@ -27,6 +27,7 @@ function App({signOut}) {
     <View className="App">
       <Card>
         <Image src={logo} className="App-logo" alt="logo" />
+		/* <h3>Hello World!</h3> */
 		<Button onClick={signOut} marginLeft="75%">Sign Out</Button>
 		<h1>AI/ML Apps Associates</h1>
       </Card>
